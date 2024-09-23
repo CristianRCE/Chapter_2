cb2e7e73a85f602b9ce9dca696ccc36be7db7879
Cristian E. Romero <cristianromero96.cr@gmail.com>
2024-09-23 02:29:04 +0000

    modularization

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
+//=====[LIBRERIES]============================================
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+//=====[Declaration of pins]============================================
+DigitalIn B1_USER(BUTTON1);
+DigitalOut LD2(LED2);
+
+//=====[Declaration and Initialization of global variables]============================================
+int i = 0;
+
+//=====[Declaration (prototypes) of publics functions]==================
+void inputsInit();
+void outputsInit();
+
+void blinkingLed();
+
+//=====[Main Function]==================
+
 int main()
 {
-    DigitalIn B1_USER(BUTTON1);
-    DigitalOut LD2(LED2);
-    
+    inputsInit();
+    outputsInit();
+
     while (true){
-        int i;
-        if(B1_USER)
+        blinkingLed();
+    }
+}
+
+void inputsInit()
+{
+    B1_USER.mode(PullDown);
+}
+
+void outputsInit()
+{
+    LD2=OFF;
+}
+
+
+void blinkingLed()
+{
+    if(B1_USER)
         {
             for(i=0;i<5;i++)
             {
@@ -18,5 +50,4 @@ int main()
                 delay(300);
             }
         }
-    }
 }
